693f3f2d0e7836e506c75f2b1b69df30d89a0fce
yassirus99 <yassir6599@gmail.com>
2021-06-17 17:15:55 -0600

    version3

--- backend_combustibles/templates/src/Components/Usuario.js
+++ backend_combustibles/templates/src/Components/Usuario.js
@@ -1,33 +1,80 @@
 import React from 'react';
 
 import{
-Grid,makeStyles,Toolbar,AppBar,Typography,Button,IconButton,Avatar
+Grid,makeStyles,AppBar,Typography,Button,Avatar,Paper,Tab,Tabs, Box
 
 }from '@material-ui/core';
 
 const useStyle = makeStyles((theme)=>({
     root: {
+      margin: theme.spacing(1)
    
-    }/*agregar estilo css*/, 
-
+    }/*agregar estilo css*/,
+    paper:{
+      padding: 20,
+      height:'100vh',
+      width:'90%',
+      margin:"20px auto",
+   },
+   Button:{
+     margin:"5%",
+     position:"relative",
+     float:"right",
+     spacing:"8%"
+    
+   },
+ 
+  
 }));
 
 
 const Usuario = () =>{
     const classes = useStyle()
+    const [value, setValue] = React.useState(2);
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+    
     return(
+<Grid container="fixed" fullWidth>
 
 <AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      
-    </IconButton>
-    <Typography variant="h5" className={classes.title}>
-      Historial de Usuarios
-    </Typography>
-    <Avatar className={classes.avatar} ></Avatar>
-  </Toolbar>
+  
+   <Avatar className={classes.avatar} ></Avatar>
+    <Typography variant="h4" className={classes.title}  >
+      Historial de Usuarios     
+    </Typography>  
 </AppBar>
+
+
+<Box m={2} pt={1} >
+      <Button variant="contained" color="primary">Ingresar Precio </Button>
+      <Button variant="contained" color="primary">Salir </Button>
+      </Box>
+   
+  
+
+  <Paper  className={classes.paper} elevation={15} >
+
+    <Grid>
+    <Paper square>
+        <Tabs
+          value={value}
+          indicatorColor="primary"
+         textColor="primary"
+          onChange={handleChange}
+          aria-label="disabled tabs example" >
+        <Tab label="Modelo Calculado" />
+        <Tab label="Graficos" />
+  </Tabs>
+</Paper>
+ </Grid>
+  
+  </Paper> 
+
+</Grid>
+
+
     );
 }
 export default Usuario;
